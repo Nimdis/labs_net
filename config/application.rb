a230dabc6637a51173b9d97973030e618e4cf3ea
Nimdis <tsaazi@gmail.com>
2014-10-06 11:06:43 +0700

    localizations; some app configs;

--- config/application.rb
+++ config/application.rb
@@ -1,23 +1,20 @@
 require File.expand_path('../boot', __FILE__)
-
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
+Bundler.require(:default, :assets, Rails.env)
 
 module LabsNet
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.time_zone = 'Novosibirsk'
+    config.i18n.available_locales = [:ru]
+    config.i18n.default_locale = :ru
+    config.i18n.locale = :ru
+    config.action_mailer.default_url_options = {
+      :host => 'tomskmedstat.ru',
+      :port => ''
+    }
+    config.autoload_paths += %W(#{config.root}/lib)
+    config.autoload_paths += Dir["#{config.root}/lib/**/"]
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
 end
