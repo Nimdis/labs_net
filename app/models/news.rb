37916f0a10263d7a235adfc04a4a5dfa690200f1
Nimdis <tsaazi@gmail.com>
2014-10-06 11:01:01 +0700

    scopes and validation for news were added

--- app/models/news.rb
+++ app/models/news.rb
@@ -1,2 +1,12 @@
 class News < ActiveRecord::Base
+  scope :sort_by_date, ->(*) { order(:created_at => :desc)  }
+  scope :last_added, ->(count=5) { order(:created_at => :desc).limit(count)  }
+
+  validates :title, :presence => true
+  validates :body, :presence => true
+  validates :short_body, :presence => true
+
+  def date
+    created_at.strftime("%B %d, %Y")
+  end
 end
